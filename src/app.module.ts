812f107cc4739581016c3ea179b001d53e583594
RuslanGon <ruslan441006@gmail.com>
2025-10-03 08:17:45 +0300

    buil base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import databaseConfig from './config/database.config';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ExampleModule } from '@modules/example/example.module';
+import { AuthModule } from './auth/auth.module'; // если у тебя уже есть модуль авторизации
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [databaseConfig],
     }),
 
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        ...config.get('database'),
-      }),
-    }),
+    // Подключаем MongoDB
+    MongooseModule.forRoot(process.env.MONGODB_URI),
 
     ExampleModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
