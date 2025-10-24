aea37172bb4daa21cc7173909be2fac80a921a6e
MRnektodotpng <android.kaplan@gmail.com>
2025-10-24 03:59:00 +0200

    feat: added swagger documentation

--- src/modules/ocr/ocr.controller.ts
+++ src/modules/ocr/ocr.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { BloodTestData } from '@common/interfaces/blood-test-data.interface';
 import { CreateOcrDto } from './dto/create.dto';
 import { OcrService } from './orc.service';
@@ -9,6 +9,11 @@ import { OcrService } from './orc.service';
 export class OcrController {
   constructor(private ocrService: OcrService) {}
 
+  @ApiOperation({ summary: 'Extraction text from image' })
+  @ApiResponse({
+    status: 201,
+    description: 'successfully received object json with data',
+  })
   @Post('extract')
   async create(@Body() createOcrDto: CreateOcrDto): Promise<BloodTestData> {
     return this.ocrService.create(createOcrDto);
