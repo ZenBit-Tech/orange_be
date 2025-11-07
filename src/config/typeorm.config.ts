8e381bbaf1c6ff2a5b42fe0697a87ddc0f6b2657
MRnektodotpng <android.kaplan@gmail.com>
2025-11-07 16:07:37 +0100

    feat: final update for deploy

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,19 @@
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
 import { User } from '@modules/user/entities/user.entity';
+import { MagicLink } from '@modules/auth/entities/magic-link.entity';
+import { Marker } from '@modules/marker/entities/marker.entity';
+
+config();
 
 export default new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT ?? '3306', 10),
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'rootroot',
-  database: process.env.DB_NAME || 'ailab',
-  entities: [User],
-  migrations: ['src/database/migrations/*.ts'],
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '3306', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [User, MagicLink, Marker],
+  migrations: [__dirname + '/../database/migrations/*.{ts,js}'],
+  synchronize: false,
 });
